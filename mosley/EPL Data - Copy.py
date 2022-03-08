709cffb0d2fa0230ee707b82bce5728c9cf4fb97
ryanmos113 <97961536+ryanmos113@users.noreply.github.com>
2022-03-08 14:05:56 +0000

    Update EPL Data - Copy.py
    not working but I can fix it g I swear

--- mosley/EPL Data - Copy.py
+++ mosley/EPL Data - Copy.py
@@ -3,15 +3,17 @@ import csv
 
 def main():
     window = Tk()
-    window.title("European Derbys Data")
+    window.title("European Derbies Data")
     window.geometry("250x250")
     window.configure(background = "black")
     window.iconbitmap("favicon.ico")
     labl = Label(window, text = "European Leagues", fg = "white", bg = "black")
     buttEPL = Button(window, text = "English Premier League", bg = "white", command = lambda: epl())
+    buttFL = Button(window, text = "French Ligue 1", bg = "white", command = lambda: fl())
 
     labl.pack()
     buttEPL.pack()
+    buttFL.pack()
     
     window.mainloop()
     
@@ -22,12 +24,12 @@ def epl():
     window.configure(background = "black")
     window.iconbitmap("favicon.ico")
     labl = Label(window, text = "English Premier League", fg = "white", bg = "black")
-    buttMan = Button(window, text = "Manchester Derby", bg = "white", command = lambda: parse("MAN UTD", "MAN CITY"))
-    buttLon = Button(window, text = "London Derby", bg = "white", command = lambda: parse("ARSENAL", "CHELSEA"))
-    buttNor = Button(window, text = "North London Derby", bg = "white", command = lambda: parse("TOTTENHAM", "ARSENAL"))
-    buttWes = Button(window, text = "West London Derby", bg = "white", command = lambda: parse("CHELSEA", "FULHAM"))
-    buttMer = Button(window, text = "Merseyside Derby", bg = "white", command = lambda: parse("LIVERPOOL", "EVERTON"))
-    buttTyn = Button(window, text = "Tyne-Wear Derby", bg = "white", command = lambda: parse("SUNDERLAND", "NEWCASTLE"))
+    buttMan = Button(window, text = "Manchester Derby", bg = "white", command = lambda: parse("MAN UTD", "MAN CITY", "EPL"))
+    buttLon = Button(window, text = "London Derby", bg = "white", command = lambda: parse("ARSENAL", "CHELSEA", "EPL"))
+    buttNor = Button(window, text = "North London Derby", bg = "white", command = lambda: parse("TOTTENHAM", "ARSENAL", "EPL"))
+    buttWes = Button(window, text = "West London Derby", bg = "white", command = lambda: parse("CHELSEA", "FULHAM", "EPL"))
+    buttMer = Button(window, text = "Merseyside Derby", bg = "white", command = lambda: parse("LIVERPOOL", "EVERTON", "EPL"))
+    buttTyn = Button(window, text = "Tyne-Wear Derby", bg = "white", command = lambda: parse("SUNDERLAND", "NEWCASTLE", "EPL"))
 
     labl.pack()
     buttMan.pack()
@@ -39,41 +41,81 @@ def epl():
     
     window.mainloop()
 
-def parse(derby1, derby2):
+def fl():
+    window = Tk()
+    window.title("French Ligue 1 Data")
+    window.geometry("300x300")
+    window.configure(background = "black")
+    window.iconbitmap("favicon.ico")
+    labl = Label(window, text = "French Ligue 1", fg = "white", bg = "black")
+    buttRho = Button(window, text = "Derby Rhone-Alpine", bg = "white", command = lambda: parse("LYON", "SAINT-ÉTIENNE", "FL"))
+    buttMan = Button(window, text = "Manchester Derby", bg = "white", command = lambda: parse("MAN UTD", "MAN CITY"))
+    buttLon = Button(window, text = "London Derby", bg = "white", command = lambda: parse("ARSENAL", "CHELSEA"))
+    buttNor = Button(window, text = "North London Derby", bg = "white", command = lambda: parse("TOTTENHAM", "ARSENAL"))
+    buttWes = Button(window, text = "West London Derby", bg = "white", command = lambda: parse("CHELSEA", "FULHAM"))
+    buttMer = Button(window, text = "Merseyside Derby", bg = "white", command = lambda: parse("LIVERPOOL", "EVERTON"))
+    buttTyn = Button(window, text = "Tyne-Wear Derby", bg = "white", command = lambda: parse("SUNDERLAND", "NEWCASTLE"))
+
+    labl.pack()
+    buttRho.pack()
+    buttLon.pack()
+    buttNor.pack()
+    buttWes.pack()
+    buttMer.pack()
+    buttTyn.pack()
+    
+    window.mainloop()
+
+def parse(derby1, derby2, league):
     pos1 = []
     pos2 = []
     total = 0
     total2 = 0
-    with open("english premier league data.csv") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            while (row[1] == derby1 or row[1] == derby2) and (row[2] == derby1 or row[2] == derby2):
-                print(f'\t{row[1]} vs {row[2]} Score: {row[3]} Year: {row[40]}')
-                if row[1] == derby1:
-                    pos1.append(row[8])
-                    pos2.append(row[9])
-                else:
-                     pos1.append(row[9])
-                     pos2.append(row[8])
+    if league == "EPL":
+        with open("english premier league data.csv") as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                while (row[1] == derby1 or row[1] == derby2) and (row[2] == derby1 or row[2] == derby2):
+                    print(f'\t{row[1]} vs {row[2]} Score: {row[3]} Year: {row[40]}')
+                    if row[1] == derby1:
+                        pos1.append(row[8])
+                        pos2.append(row[9])
+                    else:
+                         pos1.append(row[9])
+                         pos2.append(row[8])
 
-                line_count += 1
-                break
-        for i in range(len(pos1)):
-            total = total + int(pos1[i])
-            total2 = total2 + int(pos2[i])
-        avg = total / len(pos1)
-        avg2 = total2 / len(pos2)
-        avg = round(avg, 2)
-        avg2 = round(avg2, 2)
-        print(derby1, "Average Possesion: ", avg)
-        print(derby2, "Average Possession", avg2)
-        print(f'Processed {line_count} lines.')
+                    line_count += 1
+                    break
+    if league == "FL":
+        with open("french ligue1 data.csv") as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                while (row[1] == derby1 or row[1] == derby2) and (row[2] == derby1 or row[2] == derby2):
+                    print(f'\t{row[1]} vs {row[2]} Score: {row[3]} Year: {row[40]}')
+                    if row[1] == derby1:
+                        pos1.append(row[8])
+                        pos2.append(row[9])
+                    else:
+                         pos1.append(row[9])
+                         pos2.append(row[8])
+
+                    line_count += 1
+                    break
+                
+    for i in range(len(pos1)):
+        total = total + int(pos1[i])
+        total2 = total2 + int(pos2[i])
+    avg = total / len(pos1)
+    avg2 = total2 / len(pos2)
+    avg = round(avg, 2)
+    avg2 = round(avg2, 2)
+    print(derby1, "Average Possesion: ", avg)
+    print(derby2, "Average Possession", avg2)
+    print(f'Processed {line_count} lines.')
 
 main()
 
-#print(f'\t{row[1]} vs {row[2]} Score: {row[3]} Year: {row[40]}')
-
-#8 9
 
 
