16d440d16de671f73089456234d3a0c849cd3608
jackstone03 <jacks020203@gmail.com>
2022-04-04 16:54:09 +0100

    second team selection tkinter

--- EPL Data processing.py
+++ EPL Data processing.py
@@ -3,8 +3,46 @@ import csv
 import numpy as np
 import matplotlib.pylab as plt
 
-def teams2():
-    print()
+def main(teamName1, teamName2):
+    print(teamName1, " + ", teamName2)
+
+def teams2(teamName1):
+    window = Tk()
+    window.title("Premier League Data")
+    window.geometry("300x400")
+    window.configure(background = "black")
+    lab = Label(window, text = "Choose First Team", fg = "white", bg = "black")
+    butSwan =  Button(window, text = "Swansea", bg = "white", command = lambda: main(teamName1,"SWANSEA"))
+    butBurn =  Button(window, text = "Burnley", bg = "white", command = lambda: main(teamName1,"BURNLEY"))
+    butNewc =  Button(window, text = "Newcastle", bg = "white", command = lambda: main(teamName1,"NEWCASTLE"))
+    butBrom =  Button(window, text = "West Brom", bg = "white", command = lambda: main(teamName1,"WEST BROM"))
+    butWat =  Button(window, text = "Watford", bg = "white", command = lambda: main(teamName1,"WATFORD"))
+    butBrig =  Button(window, text = "Brighton", bg = "white", command = lambda: main(teamName1,"BRIGHTON"))
+    butSund =  Button(window, text = "Sunderland", bg = "white", command = lambda: main(teamName1,"SUNDERLAND"))
+    butVilla =  Button(window, text = "Aston Villa", bg = "white", command = lambda: main(teamName1,"ASTON VILLA"))
+    butCard =  Button(window, text = "Cardiff", bg = "white", command = lambda: main(teamName1,"CARDIFF"))
+    butHudd =  Button(window, text = "Huddersfield", bg = "white", command = lambda: main(teamName1,"HUDDERSFIELD"))
+    butHull =  Button(window, text = "Hull City", bg = "white", command = lambda: main(teamName1,"HULL CITY"))
+    butFul =  Button(window, text = "Fulham", bg = "white", command = lambda: main(teamName1,"FULHAM"))
+    butQpr =  Button(window, text = "QPR", bg = "white", command = lambda: main(teamName1,"QPR"))
+
+    lab.pack()
+    butSwan.pack()
+    butBurn.pack()
+    butNewc.pack()
+    butBrom.pack()
+    butWat.pack()
+    butBrig.pack()
+    butSund.pack()
+    butVilla.pack()
+    butCard.pack()
+    butHudd.pack()
+    butHull.pack()
+    butFul.pack()
+    butQpr.pack()
+
+    window.mainloop()
+    window.destroy()
 
 def teams1():
     window = Tk()
@@ -25,6 +63,7 @@ def teams1():
     butSout =  Button(window, text = "Southampton", bg = "white", command = lambda: teams2("SOUTHAMPTON"))
     butWest =  Button(window, text = "West Ham", bg = "white", command = lambda: teams2("WEST HAM"))
     butStoke =  Button(window, text = "Stoke City", bg = "white", command = lambda: teams2("STOKE"))
+    butCry =  Button(window, text = "Crystal Palace", bg = "white", command = lambda: teams2("CRYSTAL PALACE"))
 
     lab.pack()
     butManC.pack()
@@ -40,7 +79,12 @@ def teams1():
     butSout.pack()
     butWest.pack()
     butStoke.pack()
+    butCry.pack()
 
     window.mainloop()
+    window.destroy()
 
 teams1()
+
+
+
