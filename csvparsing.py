e574c5e526a655a3903c80e6496bb559638d86d4
Alex Taylor <ATaylor1@2021.ljmu.ac.uk>
2022-04-04 17:27:14 +0100

    commit

--- csvparsing.py
+++ csvparsing.py
@@ -1,6 +1,8 @@
 #Imports csv library
 import csv
+from http.client import BAD_REQUEST
 import matplotlib.pyplot as plt
+from sqlalchemy import Integer
 plt.style.use('ggplot')
 
 #Defines the subroutine csvimport
@@ -8,20 +10,102 @@ def csvimport():
     
     
     #Opens the csv file and creates a reader that puts the lines into a list variable
-    with open('english premier league data1.csv', 'r') as csvfile:
+    with open('english premier league data.csv', 'r') as csvfile:
         
         lines = csvfile.readlines()
+        goals = 0
         MANUTD = 0
+        CHEAL = 0
+        MANCIT = 0
+        ARSENAL = 0
+        LIVERPOOL = 0
+        SPURS = 0
+        LEISTER = 0
+        EVERTON = 0
+        VILLA = 0
+        SOUTHHAMPT = 0
+        WESTHAM = 0
+        NEWCASTLE = 0
+        WOLVES = 0
+        WATFORD = 0
+        CRYSTAL = 0
+        NORWICH = 0
+        LEEDS = 0
+        BRIGHTON = 0
+        BURNLEY = 0
+        WESTBROM = 0
+        
+
+
         for line in lines:
             data = line.split(',')
             if (data[1]) == "MAN UTD":
-                MANUTD = MANUTD + (data[36])
+                MANUTD = MANUTD + int(data[36])
+            elif (data[1]) == "CHELSEA":
+                CHEAL = CHEAL + int(data[36])
+               
+            elif (data[1]) == "MAN CITY":
+                MANCIT = MANCIT + int(data[36])
+                
+            elif (data[1]) == "ARSENAL":
+                ARSENAL = ARSENAL + int(data[36])
+                
+            elif (data[1]) == "LIVERPOOL":
+                LIVERPOOL = LIVERPOOL + int(data[36])
+                
+            elif (data[1]) == "TOTTENHAM":
+                 SPURS = SPURS + int(data[36])
+                
+            elif (data[1]) == "LEICESTER CITY":
+                LEISTER = LEISTER + int(data[36])
+               
+            elif (data[1]) == "EVERTON":
+                EVERTON = EVERTON + int(data[36])
+                
+            elif (data[1]) == "ASTON VILLA":
+                VILLA = VILLA + int(data[36])
+                
+            elif (data[1]) == "SOUTHHAMPTON":
+                SOUTHHAMPT = SOUTHHAMPT + int(data[36])
+                
+            elif (data[1]) == "WEST HAM":
+                WESTHAM = WESTHAM + int(data[36])
+                
+            elif (data[1]) == "NEWCASTLE":
+                NEWCASTLE = NEWCASTLE + int(data[36])
+                
+            elif (data[1]) == "WOLVES":
+                WOLVES = WOLVES + int(data[36])
+                
+            elif (data[1]) == "WATFORD":
+                WATFORD = WATFORD + int(data[36])
+               
+            elif (data[1]) == "CRYSTAL PALACE":
+                CRYSTAL = CRYSTAL + int(data[36])
+               
+            elif (data[1]) == "NORWICH":
+                NORWICH = NORWICH + int(data[36])
+              
+            elif (data[1]) == "LEEDS UTD":
+                LEEDS = LEEDS + int(data[36])
+                
+            elif (data[1]) == "BRIGHTON":
+                BRIGHTON = BRIGHTON + int(data[36])
+                
+            elif (data[1]) == "BURNLEY":
+                BURNLEY = BURNLEY + int(data[36])
+                
+            elif (data[1]) == "WEST BROM":
+                WESTBROM = WESTBROM + int(data[36])
+                
+
+    
 
 
         
     
     #Prints list of csv cells
-    print(MANUTD)
+    print(goals)
 
 
 def matplot():
