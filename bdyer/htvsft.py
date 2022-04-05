e2768b6545ec98e18919b757e29f3df1b55e0f01
braddyer01 <braddyer9801@gmail.com>
2022-04-05 03:24:00 +0100

    Code Commit Before update

--- bdyer/htvsft.py
+++ bdyer/htvsft.py
@@ -1,1 +1,20 @@
-#Code will be written here for the part written by BDYER
+from tkinter import *
+import csv
+from turtle import ht
+import matplotlib.pyplot as plt
+import numpy as np
+
+def htvsft():
+    window = Tk()
+    window.title("TempTitle")
+    window.geometry("250x250")
+    window.configure(background="black")
+
+def main():
+    mainWindow = Tk()
+    mainWindow.title("Half Time vs Full Time")
+    mainWindow.geometry("300x300")
+    mainWindow.configure(background="black")
+    mainWindow.mainloop()
+
+main()
