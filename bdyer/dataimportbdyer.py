6ebb97483bf1826b4940e954235ff332ef6eb43e
braddyer01 <braddyer9801@gmail.com>
2022-04-05 15:52:34 +0100

    Final Code Commit - v2

--- bdyer/dataimportbdyer.py
+++ bdyer/dataimportbdyer.py
@@ -1,15 +1,35 @@
 import csv
 
+
 premDataList = []
 homeTeam = []
 awayTeam = []
+allTeams = []
 ftScore = []
 htScore = []
 
-with open("english premier league data.csv") as custfile:
-    rows = csv.reader(custfile, delimiter=',')
-    for r in rows:
-        premDataList.append(r)
+def readData():
+    with open("english premier league data.csv") as custfile:
+        rows = csv.reader(custfile, delimiter=',')
+        lineNum=0
+        for r in rows:
+                homeTeam.append(r[1])
+                awayTeam.append(r[2])
+                ftScore.append(r[3])
+                htScore.append(r[4])
+                lineNum+=1
+
+print(homeTeam[1])
 
 
-print(premDataList[4])
+def getHomeAwayTeam():
+    index = 0
+    for team in homeTeam:
+        if team not in allTeams:
+            print(team)
+            allTeams.append(team)
+            index+=1
+    allTeams.remove('Home Team')
+
+getHomeAwayTeam()
+print(allTeams)
